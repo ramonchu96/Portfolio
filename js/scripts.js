5eab133b79ef7576637093d1f959365ec76ccf61
S$Code <rasoro96@gmail.com>
2023-05-28 21:13:05 +0200

    CvUpdate

--- js/scripts.js
+++ js/scripts.js
@@ -1,134 +1,22 @@
-/*!             CONFIGURATION
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
-
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#sideNav",
-    });
-})(jQuery); // End of use strict
 
 
 //-------Show section scrolling-----
 
 ScrollReveal().reveal('#about');
+ScrollReveal().reveal('#meanstacks', {delay: 500});
 ScrollReveal().reveal('#experience', {delay: 500});
 ScrollReveal().reveal('#education', {delay: 500});
 ScrollReveal().reveal('#oeducation', {delay: 500});
 ScrollReveal().reveal('#skills', {delay: 500});
 
-//Open animation left-right
-var id = null;
-function InSlideLeft() {
-  var elem = document.getElementById("animateleft");   
-  elem.style.visibility = "visible"
-  var pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 3);
-  function frame() {
-    if (pos == 300) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      //elem.style.top = pos + "px"; 
-      elem.style.left = pos + "px"; 
-    }
-  }
-}
-
-//Close animation left-right
-$("#animateleft").click(function() {
-  
-  $("#animateleft").animate({
-    position: 'center',
-    opacity: '0',
-    height:'100px',
-    width: '100px',
-    visibility: 'hidden'
-  });
-  $("#animateleft").delay(2000);
-});
-//LinkedIn Window
-function fLinkedIn (url){
-    window.open(url, "LinkedIn", "width=600, height=400")
-}
-
-//Show modal for game
-
-  $("#skills").mouseover(function(){
-    $("#exampleModal").show();
-  
-  });
-  $("#closeGame").click(function(){
-    $("#exampleModal").animate({
-      position: 'center',
-      opacity: '0',
-      height:'10px',
-      width: '10px',
-      visibility: 'hidden'
-    });
-  });
-//Show PopUps
-$(document).ready(function(){
-	 $('[data-toggle="popover1"]').popover({
-        placement:'top',
-		    trigger: 'hover',
-        html: true,
-        content: 'This is a webside based on the users needs in order to increase the number of clients in his restaurant. Also I dedicated some activities about social network (Instagram) to improve the status of the restaurant.' ,
-        opacity: '0',
-        mouse : 'active'
-    });
-    $('[data-toggle="popover1"]').delay(2000);  
-});
-$(document).ready(function(){
-  $('[data-toggle="popover2"]').popover({
-       placement:'top',
-       trigger: 'hover',
-       html: true,
-       content: 'This is a landing page where the clients are given an overview of the company and it`s services. Furthermore the clients can book an apointment and be informed about new laws.',
-       opacity: '0',
-       mouse : 'active'
-   });
-   $('[data-toggle="popover2"]').delay(2000); 
-});
 
 //ButtonNight
 function changeMode(){
   var element = document.body;  
   element.classList.toggle("darkmode");
+
+
+
 }
 
 
@@ -168,7 +56,6 @@ var language = {
     "Becario",
     subtitle1_5:
     "Becario",
-
     title2:
     "Educacion",
     title3:
@@ -179,8 +66,6 @@ var language = {
     "Habilidades",
     subtitle1:
     "Soporte IT & Desarrollador Web Junior",
-
-
     duties:
     "Cargos",
     proyects:
@@ -219,3 +104,6 @@ if (window.location.hash) {
   
 }
 
+function downloadCV(){
+  window.location.href = "https://drive.google.com/file/d/1MNYWqJVx4sxTAFDj8u-AGNFDkxQ0udhf/view?usp=sharing"
+}
